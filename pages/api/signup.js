a61ff7fd460009f7bc481c85b7a51894570fc805
Github6432 <parasshankhwar2@gmail.com>
2024-09-30 18:12:23 +0530

    customize hoodies, sticker & mugs page with resposive

---
+++ pages/api/signup.js
@@ -0,0 +1,25 @@
+import User from "@/models/User";
+import connectDb from "@/middleware/mongoose";
+
+const handler = async (req, res) => {
+    if (req.method === 'POST') {
+        const { password, repassword } = req.body
+        try {
+            console.log(req.body);
+            if (password === repassword) {
+                const user = new User(req.body);
+                await user.save();
+                res.status(200).json({ massage: 'Your Account has been created!', success: true });
+            } else {
+                res.status(400).json({ massage: 'Re-type Password is not matched ', success: false });
+            }
+        } catch (err) {
+            res.status(400).json({ massage: 'Sign Up Failed, Please try again!', success: false, err });
+        }
+    } else {
+        console.error("Error creating user:", err);
+        res.status(500).json({ message: 'Internal Server Error. Please try again later.', success: false });
+    }
+};
+
+export default connectDb(handler);
--- pages/signup.js
+++ pages/signup.js
@@ -2,71 +2,92 @@ import React, { useState } from "react";
 import Link from "next/link";
 import Company from "../public/assets/nav.png";
 import Image from "next/image";
-// import { ToastContainer, toast } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
+import { Bounce, ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Signup = () => {
   const [name, setName] = useState()
   const [email, setEmail] = useState()
   const [password, setPassword] = useState()
+  const [repassword, setRepassword] = useState()
 
   const handleChange = (e) => {
-    if(e.target.name == 'name') {
+    if (e.target.name == 'name') {
       setName(e.target.value)
     }
-    else if(e.target.name == 'email') {
+    else if (e.target.name == 'email') {
       setEmail(e.target.value)
     }
-    else if(e.target.name == 'password') {
+    else if (e.target.name == 'password') {
       setPassword(e.target.value)
+    }
+    else if (e.target.name == 're-type-password'){
+      setRepassword(e.target.value)
     }
   }
 
   const handleSubmit = async (e) => {
-      e.preventDefault()
-      const data = {name, email, password}
+    e.preventDefault()
+    const data = { name, email, password, repassword }
 
-      let res = await fetch(`http://localhost:3000/api/signup`, {
-        method: "POST", // or 'PUT'
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      })
-      let response = await res.json()
-      console.log(response)
-      
-      setName('')
-      setEmail('')
-      setPassword('')
-      
-      toast.success('Your Account has been created!', {
+    let res = await fetch(`http://localhost:3000/api/signup`, {
+      method: "POST", // or 'PUT'
+      headers: { "Content-Type": "application/json", },
+      body: JSON.stringify(data),
+    });
+    let response = await res.json();
+    const { massage, success, err } = response;
+    console.log('0', success, 'true')
+    if (success === true) {
+      console.log('1', success)
+      toast.success(massage, {
         position: "top-center",
-        autoClose: 1500,
-        hideProgressBar: true,
+        autoClose: 2000,
+        hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
         theme: "light",
-        });
+        transition: Bounce,
+      });
+    } else {
+      console.log('2', success)
+      toast.error(massage, {
+        position: "top-center",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
+    }
+
+    setName('');
+    setEmail('');
+    setPassword('');
+    setRepassword('')
+    console.log(response);
   };
 
   return (
     <div>
-       {/* <ToastContainer
-            position="top-center"
-            autoClose={1500}
-            limit={5}
-            hideProgressBar
-            newestOnTop
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="colors"
-          /> */}
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
       <div className="flex h-screen items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-md space-y-8">
           <div>
@@ -95,13 +116,10 @@ const Signup = () => {
             {/* <input type="hidden" name="remember" value="true" /> */}
             <div className="space-y-4 rounded-md shadow-sm">
               <div>
-                <label htmlFor="name" className="sr-only">
-                  Name
-                </label>
+                <label htmlFor="name" className="sr-only">Name</label>
                 <input
                   value={name || ""}
                   onChange={handleChange}
-                  
                   id="name"
                   name="name"
                   type="text"
@@ -112,9 +130,7 @@ const Signup = () => {
                 />
               </div>
               <div>
-                <label htmlFor="email" className="sr-only">
-                  Email address
-                </label>
+                <label htmlFor="email" className="sr-only">Email address</label>
                 <input
                   onChange={handleChange}
                   value={email || ""}
@@ -150,13 +166,15 @@ const Signup = () => {
                     Re-type Password
                   </label>
                   <input
+                    onChange={handleChange}
+                    value={repassword || ""}
                     id="re-type-password"
                     name="re-type-password"
                     type="password"
                     autoComplete="re-type-password"
                     required
                     className="relative block w-full appearance-none rounded-md  border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-pink-500 focus:outline-none focus:ring-pink-500 sm:text-sm"
-                    placeholder="re-type Password"
+                    placeholder="Re-type Password"
                   />
                 </div>
               </div>
